504d1e18457a1ae1a928936b8c11c2a7d7e01599
Олександр Бандалак <obandalk@hotmail.com>
2020-10-19 19:33:25 +0300

    added documentation for class TimePeriod

--- lab2/lab2-qt/timeperiod.h
+++ lab2/lab2-qt/timeperiod.h
@@ -4,20 +4,43 @@
 #include <QTime>
 #include <QString>
 #include <QFile>
-
+/*!
+* \brief Class to represent the time period
+*
+* Used for NotDisturbMode
+*
+*/
 class TimePeriod
 {
 private:
-    QString path;
-    QTime qString_to_QTime(QString line);
+    QString path;//path to file with information about 'do not disturb mode'
+    QTime qString_to_QTime(QString line);//line like hh:mm:ss
 public:
-    QTime begin;
-    QTime end;
-    bool active;
-
+    QTime begin;/*!< The begin of time period*/
+    QTime end;/*!< The end of time period*/
+    bool active;/*!< Indicates whether the time period is active*/
+/*!
+* Default constructor
+*
+*/
     TimePeriod();
+/*!
+* The constructor reads parametres from file with 'path'.
+* If is errors with file then create zero non-active time period
+*
+*/
     TimePeriod(QString path);
+/*!
+* \brief Checks whether the specified time is included in the time period
+*
+* \returns - true if now is time in time period
+* \returns - false if now isn't time in time period or time period is non-active
+*/
     bool is_period();
+/*!
+* Writes the time period to file
+*
+*/
     void write_to_file();
 };
 
