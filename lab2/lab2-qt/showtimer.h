0539f4944f8ae63fc06174db647f09e92b618343
Олександр Бандалак <obandalk@hotmail.com>
2020-10-19 19:15:29 +0300

    added documentation to class ShowTimer

--- lab2/lab2-qt/showtimer.h
+++ lab2/lab2-qt/showtimer.h
@@ -12,34 +12,65 @@
 namespace Ui {
 class ShowTimer;
 }
-
+/*!
+* \brief Class that shows the user the selected timer in a separate window
+*
+* Can pause / start / delete the timer. And also only here can edit the timer
+*
+*/
 class ShowTimer : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit ShowTimer(QVector<MyTimer*>& timers, int& indexOfCurrentTimer, QStandardItemModel *model = nullptr, QWidget *parent = nullptr);
+/*!
+* Constructor that takes a reference to array of timers,
+* reference to index of selected timer in MainWindow
+* and a pointer to model of List View in MainWindow
+*
+*/
+    explicit ShowTimer(QVector<MyTimer*>& timers, int& indexOfCurrentTimer, QStandardItemModel *model, QWidget *parent = nullptr);
     ~ShowTimer();
+/*!
+* Adds the name, time, and type of the specified timer to the window
+*
+*/
     void update_timer();
+/*!
+* \brief Changes the index of the timer
+*
+* \param indexOfTimer new index of timer that will be showed
+*
+*/
     void set_timer(int indexOfTimer);
+/*!
+* \brief Opens window with questions
+*
+* Before deleting the timer the user will receive a message with a question.
+* \param that pointer to parent QWidget
+* \param title name of message window
+* \param sentence text with questions
+* \returns - true if user pressed 'Yes'
+* \returns - false if user pressed 'No' or closed this window
+*/
     static bool questions_to_delete(QWidget* that, QString title, QString sentence);
 private slots:
-    void on_btnStart_clicked();
+    void on_btnStart_clicked();//starts the timer and changes text-color of time
 
-    void on_btnPause_clicked();
+    void on_btnPause_clicked();//pauses the timer and changes text-color of time
 
-    void on_btnEdit_clicked();
+    void on_btnEdit_clicked();//opens window to editing the timer
 
-    void on_btnDelete_clicked();
+    void on_btnDelete_clicked();//deletes the timer from 'timers' and from List View. Closes this window
 
 private:
     Ui::ShowTimer *ui;
-    QStandardItemModel *model;
-    QVector<MyTimer*>& timers;
-    int indexOfTimer;
-    int& indexOfCurrentTimer;
-    QString get_time_style();
-    QString get_timer_type();
+    QStandardItemModel *model;//reference to array of timers in MainWindow
+    QVector<MyTimer*>& timers;//pointer to model of List View in MainWindow
+    int indexOfTimer;//index of showed timer
+    int& indexOfCurrentTimer;//index of selected timer in MainWindow
+    QString get_time_style();//returns QString of time with html style
+    QString get_timer_type();//returns QString of time type
 };
 
 #endif // SHOWTIMER_H
