9251c5e21b7e069f3146885fc9504e821d4a7a77
Олександр Бандалак <obandalk@hotmail.com>
2021-02-16 20:41:54 +0200

    Added documentation for file interactive.h

--- 2-sem/lab1a/lab1a/interactive.h
+++ 2-sem/lab1a/lab1a/interactive.h
@@ -10,9 +10,23 @@
 namespace im//interactive mode
 {
 	/**
+	* \brief Starts Interactive dialog mode.
 	* 
+	* Can choose to open and save monsters or exit.
+	* 
+	* Creates a pointer to selected opening mode class.
 	*/
 	void interactive_dialog_mode();
+	/**
+	* \brief Menu with all the operations that can be performed on monsters.
+	* 
+	* \param open - pointer to selected opening mode class
+	*/
 	void interactive_menu(std::shared_ptr<om::OpeningMode> open);
+	/**
+	* \brief Menu with all searching operations.
+	* 
+	* \param open - pointer to selected opening mode class
+	*/
 	void monster_search(std::shared_ptr<om::OpeningMode> open);
 }
