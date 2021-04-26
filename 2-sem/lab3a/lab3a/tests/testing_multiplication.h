2fbe0b4d1b3ce8e2d0786f86637b2cf15d08d2f4
Megarekrut65 <bandalak28@gmail.com>
2021-04-26 16:12:52 +0300

    Added tests for multiplictions

--- 2-sem/lab3a/lab3a/tests/testing_multiplication.h
+++ 2-sem/lab3a/lab3a/tests/testing_multiplication.h
@@ -8,6 +8,15 @@ using namespace mulmatrix;
 
 TEST_CASE("testing multiplication")
 {
+	Matrix<int>(*multiply)(const Matrix<int>&, const Matrix<int>&) = &simple_multiplication<int>;
+	SUBCASE("Simple multiplication")
+	{
+		multiply = &simple_multiplication<int>;
+	}
+	SUBCASE("Strassen multiplication")
+	{
+		multiply = &strassen_multiplication<int>;
+	}
 	Matrix<int> matrix1 = { { {21,42,53},
 							  {32,63,94},
 							  {17,87,14}} },
@@ -15,8 +24,19 @@ TEST_CASE("testing multiplication")
 				matrix2 = { { {20,11,16},
 							  {13,41,28},
 							  {75,81,32}} };
-	Matrix<int> res = strassen_multiplication(matrix1, matrix2);
+	Matrix<int> res = multiply(matrix1, matrix2);
 	std::stringstream out;
 	out << res;
 	CHECK(out.str() == std::string("4941 6246 3208 \n8509 10549 5284 \n2521 4888 3156 \n"));
+	matrix2 = { { {20,11},
+				  {13,41}} };
+	CHECK_THROWS_WITH_AS(res = multiply(matrix1, matrix2),
+		"Matrices must have same size and be square!",std::logic_error);
+	matrix1 = { { {40,11,12},
+				  {33,21,12}} };
+
+	matrix2 = { { {20,11,12},
+				  {13,41,12}} };
+	CHECK_THROWS_WITH_AS(res = multiply(matrix1, matrix2),
+		"Matrices must have same size and be square!", std::logic_error);
 }
